b1ef0d6e3914f202d60136d6925ec67273b6eb06
Helderson Vieira <vieirahelder.fort@gmail.com>
2022-06-08 22:44:03 -0300

    refactor: move authusecase to its own file

---
+++ src/domain/usecases/auth-usecase.js
@@ -0,0 +1,22 @@
+const { MissingParamError, InvalidParamError } = require('../../utils/errors')
+
+module.exports = class AuthUseCase {
+  constructor(loadUserByEmailRepository) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+  }
+
+  async auth({ email, password }) {
+    if (!email) throw new MissingParamError('email')
+
+    if (!password) throw new MissingParamError('password')
+    
+    if (!this.loadUserByEmailRepository)
+      throw new MissingParamError('loadUserByEmailRepository')
+    
+      if (!this.loadUserByEmailRepository.load)
+      throw new InvalidParamError('load')
+
+    const user = await this.loadUserByEmailRepository.load(email)
+    return user || null
+  }
+}
--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,41 +1,8 @@
 const { MissingParamError, InvalidParamError } = require('../../utils/errors')
-
-class AuthUseCase {
-  constructor(loadUserByEmailRepository) {
-    this.loadUserByEmailRepository = loadUserByEmailRepository
-  }
-
-  async auth({ email, password }) {
-    if (!email) throw new MissingParamError('email')
-    if (!password) throw new MissingParamError('password')
-    if (!this.loadUserByEmailRepository)
-      throw new MissingParamError('loadUserByEmailRepository')
-    if (!this.loadUserByEmailRepository.load)
-      throw new InvalidParamError('load')
-
-    return await this.loadUserByEmailRepository.load(email)
-  }
-}
+const AuthUseCase = require('./auth-usecase');
 
 const makeSut = () => {
   const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository()
-  class AuthUseCase {
-    constructor(loadUserByEmailRepository) {
-      this.loadUserByEmailRepository = loadUserByEmailRepository
-    }
-
-    async auth({ email, password }) {
-      if (!email) throw new MissingParamError('email')
-      if (!password) throw new MissingParamError('password')
-      if (!this.loadUserByEmailRepository)
-        throw new MissingParamError('loadUserByEmailRepository')
-      if (!this.loadUserByEmailRepository.load)
-        throw new InvalidParamError('load')
-
-      const user = await this.loadUserByEmailRepository.load(email)
-      return user || null
-    }
-  }
   const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
   return {
     sut,
