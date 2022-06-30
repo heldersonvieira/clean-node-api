ef09fb8eaf7cd062b1bc79bd2e349d3d6cf37708
Helderson Vieira <vieirahelder.fort@gmail.com>
2022-06-30 00:52:40 -0300

    test: ensure Encrypter encrypt and decrypt values

--- src/shared/helpers/encrypter.spec.js
+++ src/shared/helpers/encrypter.spec.js
@@ -1,30 +1,53 @@
 const crypto = require('../utils/crypto')
 
-class Encrypter {
-  async compare(value, hash) {
-    const isValid = await crypto.compare(value, hash)
-    return isValid
+const makeSut = () => {
+  class Encrypter {
+    async compare(value, hash) {
+      const isValid = await crypto.compare(value, hash)
+      return isValid
+    }
+  
+    async encrypt(value) {
+      return await crypto.encrypt(value)
+    }
+  
+    async decrypt(value) {
+      return await crypto.decrypt(value)
+    }
   }
+  return new Encrypter()
 }
 
 describe('Encrypter', () => {
-  let password
-  let hashedPassword
+  let value
+  let hash
 
   beforeAll(async () => {
-    password = 'any_password'
-    hashedPassword = await crypto.encrypt(password)
+    value = 'any_value'
+    hash = await crypto.encrypt(value)
   })
 
   test('should return true if crypto returns true', async () => {
-    const sut = new Encrypter()
-    const isValid = await sut.compare(password, hashedPassword)
+    const sut = makeSut()
+    const isValid = await sut.compare(value, hash)
     expect(isValid).toBe(true)
   })
 
   test('should return false if crypto returns false', async () => {
-    const sut = new Encrypter()
-    const isValid = await sut.compare(password, 'hashedPassword')
+    const sut = makeSut()
+    const isValid = await sut.compare(value, 'hash')
     expect(isValid).toBe(false)
+  })
+
+  test('should return a hash', async () => {
+    const sut = makeSut()
+    const hashedValue = await sut.encrypt(value)
+    expect(hashedValue).toEqual(hash)
+  })
+
+  test('should return value decrypted iquals input value', async () => {
+    const sut = makeSut()
+    const decryptedValue = await sut.decrypt(hash)
+    expect(decryptedValue).toEqual(value)
   })
 })
