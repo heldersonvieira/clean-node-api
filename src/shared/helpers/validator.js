d6d56123ecba533baf233cd260f4d5c333de995f
Helderson Vieira <vieirahelder.fort@gmail.com>
2022-06-30 23:48:53 -0300

    feat: ensure validator throws if no email is provided

--- src/shared/helpers/validator.js
+++ src/shared/helpers/validator.js
@@ -1,5 +1,8 @@
+const { MissingParamError } = require('../errors')
+
 module.exports = class Validator {
   isEmail(email) {
+    if (!email) throw new MissingParamError('email')
     const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     return regex.test(email)
   }
--- src/shared/helpers/validator.spec.js
+++ src/shared/helpers/validator.spec.js
@@ -1,17 +1,25 @@
+const { MissingParamError } = require('../errors');
 const Validator = require('./validator');
 
-const sut = () => {
+const makeSut = () => {
   return new Validator()
 }
 
 describe('Email Validator', () => {
   test('should return true if email is valid', () => {
-    const isValidEmail = sut().isEmail('valid_email@email.com')
+    const sut = makeSut()
+    const isValidEmail = sut.isEmail('valid_email@email.com')
     expect(isValidEmail).toBe(true)
   })
 
   test('should return false if email is invalid', () => {
-    const isValidEmail = sut().isEmail('invalid_email.com')
+    const sut = makeSut()
+    const isValidEmail = sut.isEmail('invalid_email.com')
     expect(isValidEmail).toBe(false)
+  })
+
+  test('should throw if no email is provided', async () => {
+    const sut = makeSut()
+    expect(sut.isEmail).toThrow(new MissingParamError('email'))
   })
 })
