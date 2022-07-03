e8dcad6f20afac0a8f2f0f2e214ae3b5855321b8
Helderson Vieira <vieirahelder.fort@gmail.com>
2022-07-02 23:09:31 -0300

    refactor: move sut creation to a factory helper method

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -4,6 +4,7 @@ class LoadUserByEmailRepository {
   constructor(userModel) {
     this.userModel = userModel
   }
+
   async load(email) {
     const user = await this.userModel.findOne({ email })
     return user
@@ -11,6 +12,7 @@ class LoadUserByEmailRepository {
 }
 
 describe('LoadUserByEmailRepository', () => {
+  let makeSut
   let client
   let db
 
@@ -20,9 +22,18 @@ describe('LoadUserByEmailRepository', () => {
       useUnifiedTopology: true,
     })
     db = await client.db()
+
+    makeSut = () => {
+      const userModel = db.collection('users')
+      const sut = new LoadUserByEmailRepository(userModel)
+      return {
+        sut,
+        userModel,
+      }
+    }
   })
 
-  beforeEach(async () =>{
+  beforeEach(async () => {
     await db.collection('users').deleteMany()
   })
 
@@ -31,18 +42,16 @@ describe('LoadUserByEmailRepository', () => {
   })
 
   test('should return null if no user is found', async () => {
-    const userModel = await db.collection('users')
-    const sut = new LoadUserByEmailRepository(userModel)
+    const { sut } = makeSut()
     const user = await sut.load('invalid@email.com')
     expect(user).toBeNull()
   })
 
   test('should return an user if user is found', async () => {
-    const userModel = await db.collection('users')
+    const { sut, userModel } = makeSut()
     await userModel.insertOne({
       email: 'valid@email.com',
     })
-    const sut = new LoadUserByEmailRepository(userModel)
     const user = await sut.load('valid@email.com')
     expect(user.email).toBe('valid@email.com')
   })
